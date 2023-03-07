2a7f3a5f92618285a5fd9cb7879e8ef4d5dac6d7
Mark Angelo Bon <mdbon@dilg.gov.ph>
2023-03-07 16:12:21 +0800

    config main changes re RBAC

--- common/config/main.php
+++ common/config/main.php
@@ -8,15 +8,39 @@ return [
     'modules' => [
         'rbac' => [
             'class' => 'yii2mod\rbac\Module',
-        ],
-        'authManager' => [
-            'class' => 'yii\rbac\DbManager',
-            'defaultRoles' => ['guest', 'user'],
+            'as access' => [
+                'class' => yii2mod\rbac\filters\AccessControl::class
+            ],
         ],
     ],
     'components' => [
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'defaultRoles' => ['guest', 'user','admin'],
+        ],
+        'i18n' => [
+            'translations' => [
+                'yii2mod.rbac' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@yii2mod/rbac/messages',
+                ],
+                // ...
+            ],
+        ],
     ],
+    'as access' => [
+        'class' => yii2mod\rbac\filters\AccessControl::class,
+        'allowActions' => [
+            'site/*',
+            'admin/*',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
+     ],
 ];
