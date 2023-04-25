1077d241808346b78019354da93b5f53193ef195
Mark Bon <bonmarkangelo@gmail.com>
2023-04-25 14:41:12 +0800

    don't include trainee in the tag user if inactive

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -359,7 +359,7 @@ $this->title = "BPSU OJT Timesheet Monitoring System for CICT Trainees";
                 'visible' => Yii::$app->user->can('announcement-menu'),
             ],
             [
-                'label' => '<i class="fas fa-tasks"></i> Tasks '.($countTask['countTask'] ? '<i class="fas fa-paper-plane" style="color:#ffcd39;"></i>' : '<i class="fas fa-paper-plane" style="color:gray;"></i>').' '.($countReply['countTaskReplySeen'] ? '<i class="fas fa-comments" style="color:#ffcd39;"></i>' : '<i class="fas fa-comments" style="color:gray;"></i>').'', 'url' => ['/submission-thread/index'], 'active' => Yii::$app->controller->id == "submission-thread" ? true : false,
+                'label' => '<i class="fas fa-tasks"></i> Tasks '.($countTask['countTask'] ? '<i class="fas fa-paper-plane" style="color:#ffcd39;"></i>' : '').' '.($countReply['countTaskReplySeen'] ? '<i class="fas fa-comments" style="color:#ffcd39;"></i>' : '').'', 'url' => ['/submission-thread/index'], 'active' => Yii::$app->controller->id == "submission-thread" ? true : false,
                 'visible' => Yii::$app->user->can('menu-tasks'),
             ],
             ['label' => '<i class="fas fa-cogs"></i> Settings', 'url' => ['/settings'], 'active' => in_array(Yii::$app->controller->id,[
